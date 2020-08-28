33092dcdd2b6a0bf12d3aa41fcdd57ffa395c1eb
xuanzhui <daiwei1215@126.com>
2020-08-28 19:03:25 +0800

    update btc sample as pycoin api changed

--- btc_sample.py
+++ btc_sample.py
@@ -5,16 +5,20 @@ check https://github.com/richardkiss/pycoin
 pip install pycoin
 """
 
-from pycoin.key import Key
+from pycoin.key.Key import Key
 import os
 import binascii
-from pycoin.tx.Tx import Tx, SIGHASH_ALL
-from pycoin.tx.TxIn import TxIn
-from pycoin.tx.TxOut import TxOut
-from pycoin.tx.Spendable import Spendable
-from pycoin.ui import address_for_pay_to_script, script_obj_from_address
-from pycoin.tx.pay_to import build_hash160_lookup, ScriptMultisig, build_p2sh_lookup, ScriptPayToAddressWit
-from pycoin.serialize import h2b_rev
+from pycoin.coins.bitcoin.Tx import Tx
+from pycoin.satoshi.flags import SIGHASH_ALL
+from pycoin.coins.bitcoin.TxIn import TxIn
+from pycoin.coins.bitcoin.TxOut import TxOut
+from pycoin.coins.bitcoin.Spendable import Spendable
+from pycoin.coins.bitcoin.Solver import BitcoinSolver
+from pycoin.solve.utils import build_hash160_lookup, build_p2sh_lookup
+from pycoin.encoding.hexbytes import h2b_rev
+from pycoin.ecdsa.secp256k1 import secp256k1_generator
+from pycoin.networks.registry import network_for_netcode
+
 from hashlib import sha256
 import requests
 
@@ -25,10 +29,12 @@ NET_CODE = 'BTC'
 PRI_KEY_MIN = int('0x1', 16)
 PRI_KEY_MAX = int('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140', 16)
 
+network = network_for_netcode(NET_CODE)
+NORMAL_KEY = Key.make_subclass(network, secp256k1_generator)
+
 
 def address_from_pri_hex(pri_hex):
-    my_key = Key(secret_exponent=int(pri_hex, 16),
-                 prefer_uncompressed=False, netcode=NET_CODE)
+    my_key = NORMAL_KEY(secret_exponent=int(pri_hex, 16))
 
     return my_key.address()
 
@@ -46,23 +52,20 @@ def _gen_pri_key():
 
 def gen_key_pair():
     key, pri_hex = _gen_pri_key()
-    my_key = Key(secret_exponent=key,
-                 prefer_uncompressed=False, netcode=NET_CODE)
+    my_key = NORMAL_KEY(secret_exponent=key)
 
     return pri_hex, my_key.address()
 
 
 def address_from_wif(wif):
-    my_key = Key.from_text(wif)
-
+    my_key = network.parse.wif(wif)
     return my_key.address()
 
 
 def gen_key_pair_as_wif():
     key, pri_hex = _gen_pri_key()
 
-    my_key = Key(secret_exponent=key,
-                 prefer_uncompressed=False, netcode=NET_CODE)
+    my_key = NORMAL_KEY(secret_exponent=key)
 
     return my_key.wif(), my_key.address()
 
@@ -82,18 +85,18 @@ def spend_pkh_fund(tx_ins, in_keys, tx_outs):
         tx_id_b = h2b_rev(tx_id)
         _txs_in.append(TxIn(tx_id_b, idx))
 
-        _un_spent.append(Spendable(balance, script_obj_from_address(address, netcodes=[NET_CODE]).script(),
-                                   tx_id_b, idx))
+        script = network.contract.for_address(address)
+        _un_spent.append(Spendable(balance, script, tx_id_b, idx))
 
     _txs_out = []
     for balance, receiver_address in tx_outs:
-        _txs_out.append(TxOut(balance, script_obj_from_address(receiver_address, netcodes=[NET_CODE]).script()))
+        _txs_out.append(TxOut(balance, network.contract.for_address(receiver_address)))
 
     version, lock_time = 1, 0
     tx = Tx(version, _txs_in, _txs_out, lock_time)
     tx.set_unspents(_un_spent)
 
-    solver = build_hash160_lookup([int(pri_hex, 16) for pri_hex in in_keys])
+    solver = build_hash160_lookup([int(pri_hex, 16) for pri_hex in in_keys], [secp256k1_generator])
     tx.sign(solver, hash_type=SIGHASH_ALL)
 
     return tx.as_hex(), tx.id()
@@ -140,19 +143,18 @@ def recommend_satoshi_per_byte():
 
 # return address and redeem script
 def get_multisig_address(m, pub_keys):
-    pay_to_multisig_script = ScriptMultisig(m, pub_keys).script()
-    return address_for_pay_to_script(pay_to_multisig_script, netcode=NET_CODE), pay_to_multisig_script.hex()
+    pay_to_multisig_script = network.contract.for_multisig(m, pub_keys)
+    return network.address.for_p2s(pay_to_multisig_script), pay_to_multisig_script.hex()
 
 
 def gen_2of3_multisig_key_pair():
     key_pairs = []
     for i in range(0, 3):
         key, pri_hex = _gen_pri_key()
-        my_key = Key(secret_exponent=key,
-                     prefer_uncompressed=False, netcode=NET_CODE)
+        my_key = NORMAL_KEY(secret_exponent=key)
 
-        # return wif or hex format, use your own strategy
-        key_pairs.append((my_key.wif(), my_key.sec_as_hex()))
+        # return wif and pub hex format
+        key_pairs.append((my_key.wif(), binascii.hexlify(my_key.sec())))
 
     return get_multisig_address(2, [binascii.unhexlify(key[1]) for key in key_pairs]), key_pairs
 
@@ -176,40 +178,63 @@ def spend_sh_fund(tx_ins, wif_keys, tx_outs):
         tx_id_b = h2b_rev(tx_id)
         _txs_in.append(TxIn(tx_id_b, idx))
 
-        _un_spent.append(Spendable(balance, script_obj_from_address(address, netcodes=[NET_CODE]).script(),
+        _un_spent.append(Spendable(balance, network.contract.for_address(address),
                                    tx_id_b, idx))
 
     _txs_out = []
     for balance, receiver_address in tx_outs:
-        _txs_out.append(TxOut(balance, script_obj_from_address(receiver_address, netcodes=[NET_CODE]).script()))
+        _txs_out.append(TxOut(balance, network.contract.for_address(receiver_address)))
 
     version, lock_time = 1, 0
     tx = Tx(version, _txs_in, _txs_out, lock_time)
     tx.set_unspents(_un_spent)
 
     # construct hash160_lookup[hash160] = (secret_exponent, public_pair, compressed) for each individual key
-    hash160_lookup = build_hash160_lookup([Key.from_text(wif_key).secret_exponent() for wif_key in wif_keys])
+    hash160_lookup = build_hash160_lookup([network.parse.wif(wif_key).secret_exponent() for wif_key in wif_keys],
+                                          [secp256k1_generator])
 
     for i in range(0, len(tx_ins)):
         # you can add some conditions that if the input script is not p2sh type, not provide p2sh_lookup,
         # so that all kinds of inputs can work together
         p2sh_lookup = build_p2sh_lookup([binascii.unhexlify(tx_ins[i][-1])])
-        tx.sign_tx_in(hash160_lookup, i, tx.unspents[i].script, hash_type=SIGHASH_ALL, p2sh_lookup=p2sh_lookup)
+        r = BitcoinSolver(tx).solve(hash160_lookup, i, hash_type=SIGHASH_ALL, p2sh_lookup=p2sh_lookup)
+        if isinstance(r, bytes):
+            tx.txs_in[i].script = r
+        else:
+            tx.txs_in[i].script = r[0]
+            tx.set_witness(i, r[1])
 
     return tx.as_hex(), tx.id()
 
 
 def pkh_segwit_address_from_wif(wif):
     """
-    The P2SH redeemScript is always 22 bytes.
+    The redeemScript is always 22 bytes.
     It starts with a OP_0, followed by a canonical push of the keyhash (i.e. 0x0014{20-byte keyhash})
 
-    Same as any other P2SH, the scriptPubKey is OP_HASH160 hash160(redeemScript) OP_EQUAL
+    return Native SegWit (bech32): addresses start with bc1
     """
-    my_key = Key.from_text(wif)
-    script = ScriptPayToAddressWit(b'\0', my_key.hash160()).script()
-    script_hex = binascii.hexlify(script).decode()
-    return address_for_pay_to_script(script, netcode=NET_CODE), script_hex
+    my_key = network.parse.wif(wif)
+    segwit_script = network.contract.for_p2pkh_wit(my_key.hash160())
+    return network.address.for_p2pkh_wit(my_key.hash160()), segwit_script.hex()
+
+
+def get_utxo(address):
+    url = f'https://insight.bitpay.com/api/addr/{address}/utxo'
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        return None
+
+
+def broadcast_raw(raw_hex):
+    url = 'https://insight.bitpay.com/api/tx/send'
+    resp = requests.post(url, data={'rawtx': raw_hex})
+    if resp.status_code == 200:
+        return resp.text
+    else:
+        return resp.text
 
 
 if __name__ == '__main__':
@@ -217,77 +242,73 @@ if __name__ == '__main__':
     print('private key in hex format:', pri_hex)
     print('address from compressed public key:', address)
 
-    print('address from private key:', address_from_pri_hex(pri_hex))
+    # print('address from private key:', address_from_pri_hex('your private key in hex format'))
 
-    wif_key, address = gen_key_pair_as_wif()
-    print('private key in wallet import format:', wif_key)
-    print('address from compressed public key:', address)
+    # wif_key, address = gen_key_pair_as_wif()
+    # print('private key in wallet import format:', wif_key)
+    # print('address from compressed public key:', address)
 
-    print('address from private key:', address_from_wif(wif_key))
+    # print('address from private key:', address_from_wif('your private key in wif format'))
 
     # (utxo id, utxo index, balance in satoshi, sender address)
-    tx_ins = [('577ed0c91dfa959f539952e8b4e6e625473260745fd62ea6283c5c3418d94870', 0, 13122,
-               '17SRgFPdFRVdMGxcMkCBCXPvNnCPLg9gWe'),
-              ('67a690184f71ea152ece6728a85aa889d5eb735e4e2fe51c684e8ef8f933a71d', 0, 1573252,
-               '17SRgFPdFRVdMGxcMkCBCXPvNnCPLg9gWe')
-              ]
-    in_keys = ['===>hex format key for input1',
-               '===>hex format key for input2'
-               ]
-    tx_outs = [(1532624, '17SRgFPdFRVdMGxcMkCBCXPvNnCPLg9gWe'),
-               (50000, '3FMyytPExvoc8dV8oeoqoDzqGr8qeEVUGu')
-               ]
+    # tx_ins = [('tx id', idx, balance, 'sender address'),
+    #           ]
+    # in_keys = ['sender private key in hex format',
+    #            ]
+    # fee = estimate_p2pkh_tx_bytes(len(tx_ins), 2) * recommend_satoshi_per_byte()
+    # tx_outs = [(sendAmt1, 'receiver1 address'),
+    #            (balance - sendAmt - fee, 'receiver2 address')]
 
-    raw_hex, tx_id = spend_pkh_fund(tx_ins, in_keys, tx_outs)
-    print('signed raw hex:')
-    print(raw_hex)
-    print('txn id/hash:')
-    print(tx_id)
+    # raw_hex, tx_id = spend_pkh_fund(tx_ins, in_keys, tx_outs)
+    # print('signed raw hex:')
+    # print(raw_hex)
+    # print('txn id/hash:')
+    # print(tx_id)
 
-    print('calculated txn id:')
-    print(calculate_txid_from_raw_hex(raw_hex))
+    # print(broadcast_raw(raw_hex))
+
+    # print('calculated txn id:')
+    # print(calculate_txid_from_raw_hex(raw_hex))
 
     # ==============> multi sign, please pay attention here only supports inputs from the same address
-    mulsig_info, key_pairs = gen_2of3_multisig_key_pair()
-    print('address:', mulsig_info[0])
-    print('redeem script:', mulsig_info[1])
-    print('partial keys:', key_pairs)
+    # mulsig_info, key_pairs = gen_2of3_multisig_key_pair()
+    # print('address:', mulsig_info[0])
+    # print('redeem script:', mulsig_info[1])
+    # print('partial keys:', key_pairs)
 
-    redeem_script = '522103f014ab0490259b0dab5f84fa871e7e54845749d054343606813197e531a8c01d' \
-                    '210203224e6af552892d416a53be4eaae6c517d99314e9199c1d38936f2e97476690' \
-                    '2102dc5a0ba9a71cdf3fd4cba70c0994037ae4fd81b7ac48cbade1f0d73a919d86f953ae'
-    tx_ins = [('3e0594b046d2109756668d6a2d8fcf25390aeacc00f92087498e286aa8171a03', 0, 200000,
-               '3P5ifvQge9pddxVDQQJW7Byk9HKFAWiA5i',
-               redeem_script),
-              ('eb830cf7ff658f12db6cf7fd8e2b1bee2995a0c1385e65e25cfc74b311b45752', 0, 100000,
-               '3P5ifvQge9pddxVDQQJW7Byk9HKFAWiA5i',
-               redeem_script)
-              ]
-    # pick two keys from the generated key pairs
-    partial_keys = ['===>wif key1',
-                    '===>wif key2']
-    tx_outs = [(294000, '17SRgFPdFRVdMGxcMkCBCXPvNnCPLg9gWe')]
+    # redeem_script = mulsig_info[1]
+    # tx_ins = [('tx id', idx, balance, 'sender address', redeem_script),
+    #           ]
+    # pick two keys from the generated key pairs in order
+    # partial_keys = ['one of private keys in wif format',
+    #                 'one of private keys in wif format']
+    # tx_outs = [(sendAmt1, 'receiver1 address'),
+    #            (sendAmt2, 'receiver2 address'),
+    #            ]
+    #
+    # raw_hex, tx_id = spend_sh_fund(tx_ins, partial_keys, tx_outs)
+    # print('signed raw hex:')
+    # print(raw_hex)
+    # print('txn id/hash:')
+    # print(tx_id)
 
-    raw_hex, tx_id = spend_sh_fund(tx_ins, partial_keys, tx_outs)
-    print('signed raw hex:')
-    print(raw_hex)
-    print('txn id/hash:')
-    print(tx_id)
+    # # ==============> segwit native P2WPKH
+    # wif_key = '==>'
+    # address, redeem = pkh_segwit_address_from_wif(wif_key)
+    # print(address, redeem)
 
-    # ==============> segwit P2SH-P2WPKH
-    wif_key = '===>'
-    address, redeem = pkh_segwit_address_from_wif(wif_key)
-    print(address, redeem)
-    tx_ins = [('da3ad575437b093fe3d939f549dbec49be4f6b49f121a469f14ea0a9e216ed9c', 1, 50000,
-               '3FMyytPExvoc8dV8oeoqoDzqGr8qeEVUGu', '0014109031aea56f925a006847e1eece20cbd8c632fe')
-              ]
-    in_keys = [wif_key
-               ]
-    tx_outs = [(48070, '17SRgFPdFRVdMGxcMkCBCXPvNnCPLg9gWe')
-               ]
+    # tx_ins = [('tx id', idx, balance, 'sender address', 'segwit redeem script'),
+    #           ]
+    # in_keys = ['sender key in wif format',
+    #            ]
+    # tx_outs = [(sendAmt1, 'receiver1 address'),
+    #            (sendAmt2, 'receiver2 address'),
+    #            ]
+    #
+    # raw_hex, tx_id = spend_sh_fund(tx_ins, in_keys, tx_outs)
+    # print('signed raw hex:')
+    # print(raw_hex)
+    # print('txn id/hash:')
+    # print(tx_id)
 
-    raw_hex, tx_id = spend_sh_fund(tx_ins, in_keys, tx_outs)
-    print('signed raw hex:')
-    print(raw_hex)
-    print('txn id/hash:')
-    print(tx_id)
+    # print(get_utxo('13EeuKWMzgdcqFEoAmEVWVd8ZdbwjvVD5s'))
--- usdt_sample.py
+++ usdt_sample.py
@@ -8,18 +8,21 @@ usdt transaction is based on btc p2pkh transaction with OP_RETURN
 """
 
 import binascii
-from pycoin.tx.Tx import Tx, SIGHASH_ALL
-from pycoin.tx.TxIn import TxIn
-from pycoin.tx.TxOut import TxOut
-from pycoin.tx.Spendable import Spendable
-from pycoin.ui import standard_tx_out_script
-from pycoin.tx.pay_to import build_hash160_lookup
-from pycoin.serialize import h2b_rev
+from pycoin.coins.bitcoin.Tx import Tx
+from pycoin.satoshi.flags import SIGHASH_ALL
+from pycoin.coins.bitcoin.TxIn import TxIn
+from pycoin.coins.bitcoin.TxOut import TxOut
+from pycoin.coins.bitcoin.Spendable import Spendable
+from pycoin.solve.utils import build_hash160_lookup, build_p2sh_lookup
+from pycoin.encoding.hexbytes import h2b_rev
 from btc_sample import estimate_p2pkh_tx_bytes
 from btc_sample import recommend_satoshi_per_byte
+from pycoin.ecdsa.secp256k1 import secp256k1_generator
+from pycoin.networks.registry import network_for_netcode
+network = network_for_netcode("BTC")
 
 
-def tether_tx(tx_ins, private_key, send_amount, receiver):
+def tether_tx(tx_ins, private_key, send_amount, receiver, satoshi_per_byte=None):
     """
     simple usdt transaction
     here assume utxo comes from the sender and is used for mine fee
@@ -35,6 +38,7 @@ def tether_tx(tx_ins, private_key, send_amount, receiver):
     :param private_key: private key of the same sender
     :param send_amount: (display amount) * (10 ** 8)
     :param receiver: address to receive usdt
+    :param satoshi_per_byte: miner fee
     """
     _txs_in = []
     _un_spent = []
@@ -47,19 +51,22 @@ def tether_tx(tx_ins, private_key, send_amount, receiver):
         tx_id_b = h2b_rev(tx_id)
         _txs_in.append(TxIn(tx_id_b, idx))
 
-        _un_spent.append(Spendable(balance, standard_tx_out_script(address), tx_id_b, idx))
+        _un_spent.append(Spendable(balance, network.contract.for_address(address), tx_id_b, idx))
 
-    txn_fee = estimate_p2pkh_tx_bytes(len(tx_ins), 3) * recommend_satoshi_per_byte()
+    if not satoshi_per_byte:
+        satoshi_per_byte = recommend_satoshi_per_byte()
 
-    _txs_out = [TxOut(total_bal - txn_fee - 546, standard_tx_out_script(tx_ins[0][3])),
+    txn_fee = estimate_p2pkh_tx_bytes(len(tx_ins), 3) * satoshi_per_byte
+
+    _txs_out = [TxOut(total_bal - txn_fee - 546, network.contract.for_address(tx_ins[0][3])),
                 TxOut(0, binascii.unhexlify(omni_tether_script(send_amount))),
-                TxOut(546, standard_tx_out_script(receiver))]
+                TxOut(546, network.contract.for_address(receiver))]
 
     version, lock_time = 1, 0
     tx = Tx(version, _txs_in, _txs_out, lock_time)
     tx.set_unspents(_un_spent)
 
-    solver = build_hash160_lookup([int(private_key, 16)] * len(tx_ins))
+    solver = build_hash160_lookup([int(private_key, 16)] * len(tx_ins), [secp256k1_generator])
     signed_tx = tx.sign(solver, hash_type=SIGHASH_ALL)
 
     return signed_tx.as_hex(), signed_tx.id()
@@ -78,19 +85,139 @@ def omni_tether_script(amount):
     return prefix + amount_format
 
 
+def tether_tx_flush(tx_ins, private_key, send_amount, receiver):
+    _txs_in = []
+    _un_spent = []
+    total_bal = 0
+
+    for tx_id, idx, balance, address in tx_ins:
+        total_bal += balance
+
+        # must h2b_rev NOT h2b
+        tx_id_b = h2b_rev(tx_id)
+        _txs_in.append(TxIn(tx_id_b, idx))
+
+        _un_spent.append(Spendable(balance, network.contract.for_address(address), tx_id_b, idx))
+
+    txn_fee = estimate_p2pkh_tx_bytes(len(tx_ins), 2) * recommend_satoshi_per_byte()
+
+    _txs_out = [TxOut(0, binascii.unhexlify(omni_tether_script(send_amount))),
+                TxOut(total_bal - txn_fee, network.contract.for_address(receiver))]
+
+    version, lock_time = 1, 0
+    tx = Tx(version, _txs_in, _txs_out, lock_time)
+    tx.set_unspents(_un_spent)
+
+    solver = build_hash160_lookup([int(private_key, 16)] * len(tx_ins), [secp256k1_generator])
+    signed_tx = tx.sign(solver, hash_type=SIGHASH_ALL)
+
+    return signed_tx.as_hex(), signed_tx.id()
+
+
+# def tether_tx_sh(tx_ins, wif_keys, send_amount, receiver, change_address):
+#     """
+#     spend usdt having p2sh utxo
+#     :param tx_ins: list with tuple(tx_id, idx, balance, address, redeem_script),
+#         redeem_script is required for p2sh utxo, set it None for p2pkh utxo
+#     :param wif_keys: private keys of the inputs
+#     :param send_amount: (display amount) * (10 ** 8)
+#     :param receiver: address to receive usdt
+#     :param change_address: address to receive btc change
+#     """
+#     _txs_in = []
+#     _un_spent = []
+#     total_bal = 0
+#
+#     for tx_id, idx, balance, address, _ in tx_ins:
+#         total_bal += balance
+#
+#         # must h2b_rev NOT h2b
+#         tx_id_b = h2b_rev(tx_id)
+#         _txs_in.append(TxIn(tx_id_b, idx))
+#
+#         _un_spent.append(Spendable(balance, script_obj_from_address(address, netcodes=['BTC']).script(), tx_id_b, idx))
+#
+#     txn_fee = estimate_p2pkh_tx_bytes(len(tx_ins), 3) * recommend_satoshi_per_byte()
+#
+#     _txs_out = [TxOut(total_bal - txn_fee - 546, script_obj_from_address(change_address).script()),
+#                 TxOut(0, binascii.unhexlify(omni_tether_script(send_amount))),
+#                 TxOut(546, script_obj_from_address(receiver).script())]
+#
+#     version, lock_time = 1, 0
+#     tx = Tx(version, _txs_in, _txs_out, lock_time)
+#     tx.set_unspents(_un_spent)
+#
+#     # construct hash160_lookup[hash160] = (secret_exponent, public_pair, compressed) for each individual key
+#     hash160_lookup = build_hash160_lookup([Key.from_text(wif_key).secret_exponent() for wif_key in wif_keys])
+#
+#     for i in range(0, len(tx_ins)):
+#         if tx_ins[i][-1]:
+#             # p2sh_lookup = build_p2sh_lookup([binascii.unhexlify(tx_ins[i][-1])])
+#             # tx.sign_tx_in(hash160_lookup, i, tx.unspents[i].script, hash_type=SIGHASH_ALL, p2sh_lookup=p2sh_lookup)
+#             u_key = Key(secret_exponent=int('bd46417c531c36629aec7ded3c6c7ecd0acace098f47c6b9d82b77728fb477f5', 16),
+#                         prefer_uncompressed=False, netcode='BTC')
+#             tx.txs_in[i].script = u_key.sec()
+#         else:
+#             tx.sign_tx_in(hash160_lookup, i, tx.unspents[i].script, hash_type=SIGHASH_ALL)
+#
+#     return tx.as_hex(), tx.id()
+
+
 if __name__ == '__main__':
     from decimal import Decimal
+    from btc_sample import get_utxo, broadcast_raw
+    import time
 
     # (utxo id, utxo index, balance in satoshi, sender address)
-    tx_ins = [('===> utxo id', 1, 1489720,
-               '===> sender address')]
+    # tx_ins = [('xx', 1, 284538,
+    #            'xx'),
+    #           ]
+    #
+    # raw_hex, tx_id = tether_tx(tx_ins,
+    #                            'xx',
+    #                            int(Decimal('0.5') * Decimal(10 ** 8)),
+    #                            'xx')
+    #
+    # print('signed raw hex:')
+    # print(raw_hex)
+    # print('txn id/hash:')
+    # print(tx_id)
+    #
+    # print(broadcast_raw(raw_hex))
 
-    raw_hex, tx_id = tether_tx(tx_ins,
-                               '===> private key hex format',
-                               int(Decimal('0.1') * Decimal(10 ** 8)),
-                               '===> receiver address')
-
-    print('signed raw hex:')
-    print(raw_hex)
-    print('txn id/hash:')
-    print(tx_id)
+    # from pycoin.tx.pay_to import ScriptPayToScript
+    # from pycoin.key import Key
+    #
+    # u_key = Key(secret_exponent=int('bd46417c531c36629aec7ded3c6c7ecd0acace098f47c6b9d82b77728fb477f5', 16),
+    #             prefer_uncompressed=False, netcode='BTC')
+    # u_wif = u_key.wif()
+    #
+    # script = ScriptPayToScript(u_key.hash160())
+    # # print(script.address())
+    # redeem = binascii.hexlify(script.script()).decode()
+    # print(binascii.hexlify(u_key.sec()))
+    #
+    # b_key = Key(secret_exponent=int('9e50b3e179eda7d988db33919ebac1464710de58cae1862c3e853c90692e3e76', 16),
+    #             prefer_uncompressed=False, netcode='BTC')
+    # b_wif = b_key.wif()
+    # # print(b_key.address())
+    # # print(b_wif)
+    #
+    # tx_ins = [('90c54d534ba78b30f2bb2dd0adb38cc7fbe48e8ce429068dc2135a69740f8067', 2, 540,
+    #            '3Kuqjvvz81aRdiYnSJHSx5re2cCh1z3qfW', binascii.hexlify(u_key.sec())),
+    #           ('95fee62879f768184254f5775352d4743460223ae269547c03a215ed6656b74f', 0, 139986,
+    #            '1PYQsxk5GoWc3bL9TQMQwpvnHWa6btetUK', None)
+    #           ]
+    # in_keys = [u_wif,
+    #            b_wif
+    #            ]
+    #
+    # raw_hex, tx_id = tether_tx_sh(tx_ins,
+    #                               in_keys,
+    #                               int(Decimal('0.1') * Decimal(10 ** 8)),
+    #                               '17LNm71xoEvF5h3KuqhcNAsCnPnFSDWUJT',
+    #                               '1PYQsxk5GoWc3bL9TQMQwpvnHWa6btetUK')
+    # print('signed raw hex:')
+    # print(raw_hex)
+    # print('txn id/hash:')
+    # print(tx_id)
